5f8e8746e7ff851b2f88ff384bfb77dd6ae23af3
Boggdan <bandabog@gmail.com>
2021-12-22 00:31:56 -0600

    Add encoding as configuration parameter Fixes #2

--- tap_s3_csv/csv_handler.py
+++ tap_s3_csv/csv_handler.py
@@ -29,8 +29,14 @@ def get_row_iterator(table_spec, file_handle):
     # we use a protected member of the s3 object, _raw_stream, here to create
     # a generator for data from the s3 file.
     # pylint: disable=protected-access
+    if 'encoding' in table_spec:
+        encoding = table_spec['encoding']
+    else:
+        encoding = 'utf-8'
+
+
     file_stream = codecs.iterdecode(
-        file_handle._raw_stream, encoding='utf-8')
+        file_handle._raw_stream, encoding=encoding)
 
     field_names = None
     delimiter = None
@@ -40,6 +46,28 @@ def get_row_iterator(table_spec, file_handle):
     if 'delimiter' in table_spec:
         delimiter = table_spec['delimiter']
 
-    reader = csv.DictReader(file_stream, fieldnames=field_names, delimiter=delimiter)
+    reader = csv.DictReader(file_stream, fieldnames=field_names, delimiter=delimiter)    
+    
+    doublequote = None
+    quotechar = None
+    quoting = None
+
+    if 'doublequote' in table_spec:
+        doublequote = table_spec['doublequote']
+    if 'quotechar' in table_spec:
+        quotechar = table_spec['quotechar']
+    if 'quoting' in table_spec:
+        quoting = table_spec['quoting']
+
+    if doublequote != None and quotechar != None and quoting != None:
+        if quoting == 'csv.QUOTE_NONE':
+            reader = csv.DictReader(file_stream, fieldnames=field_names, delimiter=delimiter, doublequote=doublequote, quotechar=quotechar, quoting=csv.QUOTE_NONE)
+        if quoting == 'csv.QUOTE_MINIMAL':
+            reader = csv.DictReader(file_stream, fieldnames=field_names, delimiter=delimiter, doublequote=doublequote, quotechar=quotechar, quoting=csv.QUOTE_MINIMAL)
+        if quoting == 'csv.QUOTE_ALL':
+            reader = csv.DictReader(file_stream, fieldnames=field_names, delimiter=delimiter, doublequote=doublequote, quotechar=quotechar, quoting=csv.QUOTE_ALL)
+        if quoting == 'csv.QUOTE_NONNUMERIC':
+            reader = csv.DictReader(file_stream, fieldnames=field_names, delimiter=delimiter, doublequote=doublequote, quotechar=quotechar, quoting=csv.QUOTE_NONNUMERIC)
+
 
     return generator_wrapper(reader)
